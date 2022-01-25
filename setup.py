859b017354e8d089c0c3e4723af71e292d8fc5d1
Fabrice SALVAIRE <fabrice.salvaire@orange.fr>
2022-01-25 04:37:23 +0100

    setup

---
+++ setup.py
@@ -0,0 +1,39 @@
+#! /usr/bin/env python3
+
+####################################################################################################
+#
+# Avalanche - 
+# Copyright (C) 2021 Fabrice Salvaire
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Affero General Public License for more details.
+#
+# You should have received a copy of the GNU Affero General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+#
+####################################################################################################
+
+####################################################################################################
+
+import sys
+
+from setuptools import setup
+
+####################################################################################################
+
+# Check for python3 setup.py install
+required_python_version = (3, 10)
+if sys.version_info < required_python_version:
+    sys.stderr.write('ERROR: PySpice requires Python {}.{}\n'.format(*required_python_version))
+    sys.exit(1)
+
+####################################################################################################
+
+setup()
