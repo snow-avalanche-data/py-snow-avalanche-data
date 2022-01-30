b49a63856862125c049831f40d34944c0720aa9e
Fabrice SALVAIRE <fabrice.salvaire@orange.fr>
2022-01-30 02:11:10 +0100

    fix plot

--- SnowAvalancheData/Plot/__init__.py
+++ SnowAvalancheData/Plot/__init__.py
@@ -36,6 +36,12 @@ class Figure:
     A4 = (297, 210)
     A5 = (A4[1], A4[0]//2)   # 148
 
+    Y_UNIT_MAP = {
+        UnitType.COUNT: '#',
+        UnitType.NORMALISED: '%',
+        UnitType.PERCENT: '%',
+    }
+
     POOL = {}
 
     ##############################################
@@ -116,6 +122,10 @@ class Figure:
     def bar(self, histogram: EnumHistogram, title: str=None, axe: list=None) -> None:
         ax = self._get_axe(axe)
 
+        ax.set_title(title or histogram.title)
+        ax.set_ylabel(self.Y_UNIT_MAP[histogram.y_unit])
+        ax.grid(True)
+
         indexes, y, y_errors = histogram.to_graph()
 
         # print('---')
@@ -135,9 +145,7 @@ class Figure:
         # print(indexes, labels)
 
         ax.bar(indexes, y, width=.3, yerr=y_errors)   # , label=title
-        ax.set_title(title or histogram.title)
         ax.set_xticks(indexes, labels=labels)
-        ax.grid(True)
         if has_long_label:
             # Fixme: -> func
             for tick in ax.get_xticklabels():
@@ -148,27 +156,22 @@ class Figure:
     def histogram(self, histogram: Histogram, title: str=None, axe: list=None) -> None:
         ax = self._get_axe(axe)
 
+        ax.set_title(title or histogram.title)
+        ax.set_xlabel(histogram.unit)
+        ax.set_ylabel(self.Y_UNIT_MAP[histogram.y_unit])
+        ax.grid(True)
+
         x, y, x_errors, y_errors, edges = histogram.to_graph(non_null=False)
         ax.stairs(y, edges, fill=True)
         x, y, x_errors, y_errors = histogram.to_graph()
         ax.errorbar(x, y, y_errors, fmt='o', linewidth=2, capsize=6)
-
-        ax.set_title(title or histogram.title)
-        ax.set_xlabel(histogram.unit)
-        y_unit_map = {
-            UnitType.COUNT: '#',
-            UnitType.NORMALISED: '%',
-            UnitType.PERCENT: '%',
-        }
-        ax.set_ylabel(y_unit_map[histogram.y_unit])
-        ax.grid(True)
 
         binning = histogram.binning
         # x_ticks = np.arange(binning.interval.inf, binning.interval.sup, binning.bin_width)
         offset = binning.bin_width / 2
         inf = x[0]-offset
         sup = x[-1]+3*offset
-        x_ticks = np.arange(inf, sup, step=binning.bin_width)
+        x_ticks = binning.bins()   # np.arange(inf, sup, step=binning.bin_width)
         ax.set_xticks(x_ticks)
         # Fixme: inf!
         # ax.set_xlim(0, sup)
@@ -183,38 +186,46 @@ class Figure:
 
     def bar_number(self, histogram: Histogram, title: str=None, axe: list=None) -> None:
         ax = self._get_axe(axe)
+        ax.set_title(title or histogram.title)
+        ax.set_ylabel(self.Y_UNIT_MAP[histogram.y_unit])
+        ax.grid(True)
 
         x, y, x_errors, y_errors = histogram.to_graph(centred=False)
         labels = [str(int(_)) for _ in x]
         indexes = np.arange(0, len(x))
 
         ax.bar(indexes, y, yerr=y_errors, width=.3)
-        ax.set_title(title or histogram.title)
         ax.set_xticks(indexes, labels=labels)
-        ax.grid(True)
 
     ##############################################
 
     def polar_bar(self, histogram: Histogram, title: str=None, axe: list=None) -> None:
-        indexes, y, y_errors = histogram.to_graph()
         ax = self._get_axe(axe)
         ax.remove()
         ax = self._figure.add_subplot(self._number_of_rows, self._number_of_columns, self._location +1, projection='polar')
+
+        ax.set_title(title or histogram.title)
+        ax.grid(True)
+
+        indexes, y, y_errors = histogram.to_graph()
         # theta = np.linspace(0, 2*np.pi, 8, endpoint=False)
         theta = np.array([90, 45, 0, 315, 271, 225, 181, 135]) * 2*np.pi / 360
         ax.bar(theta, y, yerr=y_errors, width=(np.pi/4 * .90), color='tab:blue', alpha=.8)
         # ax.bar(theta, y + y_errors, width=(np.pi/4 * .90), color='tab:blue', alpha=.8)
         # ax.bar(theta, y, width=(np.pi/4 * .90), color='tab:blue', alpha=1.0)
         # ax.bar(theta, y - y_errors, width=(np.pi/4 * .90), color='white', alpha=.8)
-        ax.set_title(title or histogram.title)
         ax.set_xticks(theta, labels=histogram.labels)
-        ax.grid(True)
 
     ##############################################
 
     def box_plot(self, histogram: Histogram2D, title: str, axe: list=None) -> None:
         """Also called Hinton diagram"""
         ax = self._get_axe(axe)
+
+        ax.set_title(title or histogram.title)
+        ax.set_xlabel(histogram.x_label)
+        ax.set_ylabel(histogram.y_label)
+
         if np.min(histogram.accumulator) < 0:
             raise ValueError("Bin contents must be positive")
 
@@ -226,17 +237,17 @@ class Figure:
 
         for (x, y), size in np.ndenumerate(accumulator):
             rectangle = plt.Rectangle([x - size / 2, y - size / 2], size, size,
-                                 edgecolor='black', facecolor='white')
+                                      edgecolor='black', facecolor='white')
             ax.add_patch(rectangle)
 
         ax.set_aspect('equal', 'box')
         x_size, y_size = accumulator_view.shape
         ax.set_xlim(-.5, x_size)
         ax.set_ylim(-.5, y_size)
-        labels = [str(x) for x in histogram.binning.x.bin_centers()]
-        ax.set_xticks(np.arange(x_size) +.5, labels, rotation='vertical')
-        labels = [str(x) for x in histogram.binning.y.bin_centers()]
-        ax.set_yticks(np.arange(y_size) +.5, labels)
+        labels = [f'{_:g}' for _ in histogram.binning.x.bins()]
+        ax.set_xticks(np.arange(x_size +1) -.5, labels, rotation='vertical')
+        labels = [f'{_:g}' for _ in histogram.binning.y.bins()]
+        ax.set_yticks(np.arange(y_size +1) -.5, labels)
 
     ##############################################
 
